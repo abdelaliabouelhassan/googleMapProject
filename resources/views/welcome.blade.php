804d9d5764b2862a11164aa93d5e8b4b3aba5e44
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2022-05-08 04:26:01 +0100

    finishing mering vue js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,8 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div id="app">
-            
-                <hello-world/>
-           
-        </div>
+        <div id="app"></div>
 
-        <script src="{{ mix('js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('{path}', function () {
     return view('welcome');
 })->where('path', '[a-zA-Z0-9-/]+');
+
+
